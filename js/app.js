ee54b561e8aba97fc2004ba0a717d11f45fa65f5
Josh Doody <joshuadoody@gmail.com>
2016-04-01 15:44:46 -0400

    added new version of Levels with multiple-choice tiers

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 (function() {
   (function($) {
     return $(function() {
+      $(".js-multiple-choice").click(function(event) {
+        var $button, choices_id;
+        $button = $(this);
+        choices_id = $button.attr('data-multiple-choices-id');
+        $('#' + choices_id).toggle();
+        $(".package__feature-details").hide();
+        $(".js-expand").removeClass('expanded');
+        event.preventDefault();
+        return event.stopPropagation();
+      });
+      $(".js-multiple-choice-close").click(function(event) {
+        event.preventDefault();
+        return $(event.target).parents(".package__multiple-choices").hide();
+      });
       $(".js-expand").click(function(event) {
         var $clicked, $details, expandId, offset;
         $clicked = $(event.target).closest('a');
@@ -37,6 +51,12 @@
       });
       $("body").click(function(event) {
         if (!$(event.target).parents('.package__feature-details').length) {
+          $(".package__feature-details").hide();
+          $(".js-expand").removeClass('expanded');
+          $(".package__multiple-choices").hide();
+        }
+        if (!$(event.target).parents('.package__multiple-choices').length) {
+          $(".package__multiple-choices").hide();
           $(".package__feature-details").hide();
           return $(".js-expand").removeClass('expanded');
         }
