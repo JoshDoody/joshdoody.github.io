ea5a9cbbf4e24b12c88728ce74bf2b72b4b0036c
Josh Doody <joshuadoody@gmail.com>
2016-11-09 20:31:28 -0500

    trying to get DPT to work for gynr module-1 page

---
+++ js/drip_pro_init.js
@@ -0,0 +1,33 @@
+---
+---
+$(function(){
+   (new DripPro).init({
+		 template_path: '/ads/',
+	   post_drip_response: {{ page.title }}
+	 })
+}) 
+	 
+window.drip_plinko = function(drip) {
+  if (drip.is_anon) {
+    return {
+			// offer: 'promotion-course',
+      // modal: 'promotion-course-modal'
+    }
+  } else {
+		return {
+			
+		}
+    /* if (drip.has_tag('Customer')) {
+      return {
+        offer: 'show_love',
+        footer: 'we-love-our-customers'
+      }
+    } else {
+      return {
+        offer: 'product-ad',
+        sidebar: 'product-sidebar',
+        footer: 'product-footer'
+      }
+    } */
+  }
+}
