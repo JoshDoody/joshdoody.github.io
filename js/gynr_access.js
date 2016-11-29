cc78a658a8354b551fd87cdc5c38b34fcd105434
Josh Doody <joshuadoody@gmail.com>
2016-11-29 11:52:47 -0500

    GYNR_access.js looking at test tag; /estimate only showing stuff to customers

--- js/gynr_access.js
+++ js/gynr_access.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
 				$(this).show();
 			})
 		} else {
-			if(dripUser.rawDripContact.tags.indexOf('Purchased - GYNR') !== -1){
+			if(dripUser.rawDripContact.tags.indexOf('Purchased - Get Your Next Raise - Test') !== -1){
 				console.log("Subscriber, Tagged");
 				$(".gynr-customer").each(function () {
 					$(this).show();
