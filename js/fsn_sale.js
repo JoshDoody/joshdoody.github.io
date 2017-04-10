80a74837bde2ab2806158d388d42efc461ffc2c0
Josh Doody <joshuadoody@gmail.com>
2017-04-10 11:24:52 -0700

    testimonials updated; fsn_sale simplified so always shows sale even after purchase

--- js/fsn_sale.js
+++ js/fsn_sale.js
@@ -4,24 +4,25 @@ $(document).ready(function () {
 		if(dripUser.isAnon) {
 			console.log("FSN_sale: Anonymous");
 			// Do nothing
-		} else {
-			if(dripUser.rawDripContact.tags.indexOf('Purchased - FSN - Bundle') !== -1){
-				console.log("FSN_sale: Customer");
+			// commenting out 'if' because it's not bad to keep running the sale
+			// after they buy - what if they want to upgrade?
+			// if(dripUser.rawDripContact.tags.indexOf('Purchased - FSN - Bundle') !== -1){
+			//	console.log("FSN_sale: Customer");
+			// next section is ... } else if() { ... when this if is included
 				// Do nothing - they already bought
-			} else if(dripUser.rawDripContact.custom_fields && dripUser.rawDripContact.custom_fields.fsn_offer_expires){
-				console.log("FSN_sale: Subscriber");
-				var now = Math.floor(Date.now() / 1000);
-				var expiry = dripUser.rawDripContact.custom_fields.fsn_offer_expires;
-				var remaining = expiry - now;
-				console.log(remaining);
-				if(remaining > 0){
-					$('.fsn-full-price').hide();
-					var clock = $('.clock').FlipClock(remaining, {
-			        clockFace: 'DailyCounter',
-			        countdown: true
-			    })
-					$('.fsn-sale-price').show();
-				}
+		} else if(dripUser.rawDripContact.custom_fields && dripUser.rawDripContact.custom_fields.fsn_offer_expires){
+			console.log("FSN_sale: Subscriber");
+			var now = Math.floor(Date.now() / 1000);
+			var expiry = dripUser.rawDripContact.custom_fields.fsn_offer_expires;
+			var remaining = expiry - now;
+			console.log(remaining);
+			if(remaining > 0){
+				$('.fsn-full-price').hide();
+				var clock = $('.clock').FlipClock(remaining, {
+		        clockFace: 'DailyCounter',
+		        countdown: true
+		    })
+				$('.fsn-sale-price').show();
 			}
 		}
 	};
