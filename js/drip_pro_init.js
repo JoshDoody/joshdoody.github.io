d743911c22ae2d59608a2867cfefaf3c604ec1cd
Josh Doody <joshuadoody@gmail.com>
2018-06-18 19:24:20 -0400

    removed root url from tag for Read -

--- js/drip_pro_init.js
+++ js/drip_pro_init.js
@@ -167,8 +167,12 @@ jQuery(function(){
       traits = $.extend(traits, this.custom_fields())
 
 			//var pageName = document.title.replace(' | Fearless Salary Negotiation','').replace(' | Get Your Next Raise','')
-			var pageURL = window.location.origin + window.location.pathname
-      if (pageURL.length && window.location.origin != 'http://localhost:4000') {
+			//var pageURL = window.location.origin + window.location.pathname
+			
+			//removed window.location.origin to keep tags cleaner
+			var pageURL = window.location.pathname
+      
+			if (pageURL.length && window.location.origin != 'http://localhost:4000') {
         this.add_tag('Read - ' + pageURL)
 				console.log("Tags: " + this.tags())
       }
