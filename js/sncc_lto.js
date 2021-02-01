9dfc4075419047443ec37a2a69579cd0518c57e2
Josh Doody <josh@Joshs-MacBook-Pro.local>
2021-02-01 16:53:38 -0500

    countdown timer for /thanks/negotiate and /thanks/interview redirects after it hits zero OR doesn't show sale if page loaded during another time

--- js/sncc_lto.js
+++ js/sncc_lto.js
@@ -1,18 +1,38 @@
 $(document).ready(function(){
 	var now = Math.floor(Date.now() / 1000);
 	var expiry = now + 1800;
-	var remaining = expiry - now;
-	console.log(remaining);
-	if(remaining > 0){
+	
+	if (Cookie.exists('expiry')) {
+		console.log('already exists');
+		var good_until = Cookie.get('expiry');
+		var remaining = good_until - now;
+		
+		if(remaining > 0){
+			$('.fsn-full-price').hide();
+	    jQuery(function(){
+	        jQuery('#countdownTimer').DYMTimer({
+	            endTime: good_until,
+	            endMessage: "We're redirecting you now!",
+		          template: '<p><strong>DONT WAIT! YOUR CHANCE TO SAVE $30 ENDS IN...</strong> <strong>{{MIN}}</strong> min <strong>{{SEC}}</strong> SEC</p>',
+	            redirectUrl: 'https://fearlesssalarynegotiation.com/salary-negotiation-video-course/'
+	        });
+	    });
+			$('.fsn-sale-price').show();
+		 }
+	} else {
+		console.log('no cookie');
+		Cookie.set('expiry', expiry, {
+		  expires: 3
+		});
 		$('.fsn-full-price').hide();
     jQuery(function(){
         jQuery('#countdownTimer').DYMTimer({
             endTime: expiry,
             endMessage: "We're redirecting you now!",
 	          template: '<p><strong>DONT WAIT! YOUR CHANCE TO SAVE $30 ENDS IN...</strong> <strong>{{MIN}}</strong> min <strong>{{SEC}}</strong> SEC</p>',
-            redirectUrl: 'https://fearlesssalarynegotiation.com/get-started/'
+            redirectUrl: 'https://fearlesssalarynegotiation.com/salary-negotiation-video-course/'
         });
     });
 		$('.fsn-sale-price').show();
-	 }
+	}
  });
